0b2788fa6fc93fab479f2915c9cd715c2a9a12fb
Brandon Hilkert <brandonhilkert@gmail.com>
2018-04-25 20:49:25 -0400

    Fill out info section

--- lib/omniauth/strategies/microsoft_v2_auth.rb
+++ lib/omniauth/strategies/microsoft_v2_auth.rb
@@ -13,13 +13,24 @@ module OmniAuth
         token_url:     '/common/oauth2/v2.0/token'
       }
 
-     option :authorize_options, [:scope]
+      option :authorize_options, [:scope]
 
       uid { raw_info["id"] }
 
       extra do
         {
-          'raw_info' => raw_info
+          'raw_info' => raw_info,
+          'params'   => access_token.params
+        }
+      end
+
+      info do
+        {
+          email:      raw_info["mail"] || raw_info["userPrincipalName"],
+          first_name: raw_info["givenName"],
+          last_name:  raw_info["surname"],
+          name:       full_name,
+          nickname:   raw_info["userPrincipalName"],
         }
       end
 
@@ -43,6 +54,9 @@ module OmniAuth
         options[:redirect_uri] || (full_host + script_name + callback_path)
       end
 
+      def full_name
+        raw_info.values_at("givenName", "surname").compact.join(' ')
+      end
     end
   end
 end
